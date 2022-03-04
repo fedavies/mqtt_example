daafb0518d6990d5c9a08cbb08a2c70c48ad27b6
Fraser <fraserdavies3@gmail.com>
2022-03-04 22:56:11 +0000

    Middle of attempt to publish JSON. Time for bed

--- mqtt_demo.c
+++ mqtt_demo.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include "mosquitto.h"
 #define TRUE 1
 #define FALSE 0
 
-static enum STATUS{SUCCESS, FAILURE};
+static enum STATUS {SUCCESS = 0, FAILURE = 1};
+
+typedef struct {
+  char time[5];
+  int temp;
+} Message;
 
 /**************************************************************************/
 
@@ -49,9 +56,38 @@ static enum STATUS initialise ()
 
 /**************************************************************************/
 
+static Message *create_message ()
+{
+  Message *mess;
+  time_t raw_time;
+  struct tm *timeinfo;
+
+  mess = malloc (sizeof (Message));
+  if (!mess)
+    return NULL;
+
+  time (&raw_time);
+  time_info = localtime (&rawtime);
+
+  mess.time = asctime (timeinfo);
+  /* Create a random temperature that is at least reasonable for a rooom */
+  mess.temp = rand () % 30 + 5;
+
+  return mess;
+}
+
+/**************************************************************************/
+
+static void destroy_message (Message *message)
+{
+  free (message);
+}
+
+/**************************************************************************/
+
 static enum STATUS publish_message (const char *host,
                                const int   port,
-                               const char *message,
+                               const Message *message,
                                const char *topic,
                                const int   qos)
 {
@@ -77,10 +113,6 @@ static enum STATUS publish_message (const char *host,
     return FAILURE;
 
 
-  status = mosquitto_loop (publisher , -1, 1);
-  if (status != MOSQ_ERR_SUCCESS)
-    return FAILURE;
-
 
   mosquitto_destroy (publisher);
 
@@ -103,15 +135,22 @@ int main (int argc, char *argv[])
   char *host = "127.0.0.1";
   int port = 1883;
   int qos = 1;
-  const char *message = "A message from beyond";
   const char *topic = "house/sensor";
+#if 0
+  const char *message = "A message from beyond\n";
+#else
+  Message *message;
+#endif
+
 
 
   status = initialise ();
   CHECK_STATUS (status);
 
+  message = create_message ();
   status = publish_message (host, port, message, topic, qos);
   CHECK_STATUS (status);
+  destroy_message (message);
 
   clean_up ();
 
