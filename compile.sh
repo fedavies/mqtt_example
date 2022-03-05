139223bd2873053c6fba2b4f0d58b469145df3a3
Fraser <fraserdavies3@gmail.com>
2022-03-05 23:49:01 +0000

    Added a little script file to sort everything out - download mosquitto now

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+sudo pacman -S mosquitto
 mosquitto -d
 gcc -Wall -g mqtt_publisher.c mqtt_broker_interface.c -lmosquitto --output mqtt_publisher
 gcc -Wall -g mqtt_subscriber.c mqtt_broker_interface.c -lmosquitto --output mqtt_subscriber
