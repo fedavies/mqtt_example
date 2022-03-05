2f793255079e38a40d2f797819158eae02ad7d58
Fraser <fraserdavies3@gmail.com>
2022-03-05 20:44:37 +0000

    (Finally) got the json to be sent. Could do with using malloc though

--- mqtt_demo.c
+++ mqtt_demo.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -8,12 +9,8 @@
 
 static enum STATUS {SUCCESS = 0, FAILURE = 1};
 
-typedef struct {
-  char time[5];
-  int temp;
-} Message;
-
-typedef char * JSON;
+/* ERGH! I couldn't get malloc to behave, so this will have to do for now */
+static char json[17];
 
 /**************************************************************************/
 
@@ -58,18 +55,19 @@ static enum STATUS initialise ()
 
 /**************************************************************************/
 
-static char numbers[100] =
-  { 00, 01, 02, 03, 04, 05, 06, 07, 08, 09,
-    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-    40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-    70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-    80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-    90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
-  }
+/* Saves getting into complicated memory clean-up, if slightly inelegant. */
+static char *_numbers[100]=
+  { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+    "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+    "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+    "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+    "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+    "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+    "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+    "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+    "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+    "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
+  };
 
 /**************************************************************************/
 
@@ -84,33 +82,32 @@ static char *itoa_2 (int a)
 }
 /**************************************************************************/
 
-static JSON *convert_message_to_JSON (char *time, int temp)
+static char *convert_message_to_JSON (char *time, int temp)
 {
-  JSON *json;
 
-  json = malloc (16 * sizeof (char));
-  if (!json)
-    return NULL;
+  memset (&json[0], '{', 1);
+  memset (&json[1], '\n', 1);
 
-  json[0] = "{";
-  json[1] = "\n";
+  memset (&json[2], '\"', 1);
+  memset (&json[3], time[0], 1);
+  memset (&json[4], time[1], 1);
+  memset (&json[5], ':', 1);
+  memset (&json[6], time[2], 1);
+  memset (&json[7], time[3], 1);
+  memset (&json[8], '\"', 1);
 
-  json[2] = "\"";
-  json[3] = "time[0]";
-  json[4] = "time[1]";
-  json[5] = ":";
-  json[6] = "time[2]";
-  json[7] = "time[3]";
-  json[8] = "\"";
-
-  json[9] = " ";
-  json[10] = ":";
-  json[11] = " ";
+  memset (&json[9], ' ', 1);
+  memset (&json[10], ':', 1);
+  memset (&json[11], ' ', 1);
 
   memcpy (&json[12], itoa_2 (temp), 2);
 
-  json[14] = "\n";
-  json[15] = "}";
+  memset (&json[14], '\n', 1);
+  memset (&json[15], '}', 1);
+
+  memset (&json[16], '\0', 1);
+
+  printf ("%s\n", json);
 
   return json;
 
@@ -118,43 +115,41 @@ static JSON *convert_message_to_JSON (char *time, int temp)
 
 /**************************************************************************/
 
-static JSON *create_message ()
+static char *create_message ()
 {
-  time_t raw_time;
+  time_t rawtime;
   struct tm *timeinfo;
-  char time[4];
+  char clock_time[4];
   int temp;
+  char *message;
 
-  mess = malloc (sizeof (Message));
-  if (!mess)
-    return NULL;
+  time (&rawtime);
+  timeinfo = localtime (&rawtime);
 
-  time (&raw_time);
-  time_info = localtime (&rawtime);
-
-  mess.time = time_info;
   /* Create a random temperature that is at least reasonable for a rooom */
   temp = rand () % 30 + 5;
 
-  memcpy (&time[0], itoa_2 (time_info.tm_hour), 2);
-  memcpy (&time[2], itoa_2 (time_info.tm_min), 2);
-  convert_message_to_JSON (time, temp);
+  memcpy (&clock_time[0], itoa_2 (timeinfo -> tm_hour), 2);
+  memcpy (&clock_time[2], itoa_2 (timeinfo -> tm_min), 2);
+  message = convert_message_to_JSON (clock_time, temp);
+  if (!message)
+    return NULL;
 
-  return mess;
+  return message;
 }
 
 /**************************************************************************/
 
-static void destroy_message (Message *message)
+static void destroy_message (char *message)
 {
-  free (message);
+  /* do nothing - the json is a global array */
 }
 
 /**************************************************************************/
 
 static enum STATUS publish_message (const char *host,
                                     const int   port,
-                                    const Message *message,
+                                    char *message,
                                     const char *topic,
                                     const int   qos)
 {
@@ -207,7 +202,7 @@ int main (int argc, char *argv[])
 #if 0
   const char *message = "A message from beyond\n";
 #else
-  Message *message;
+  char *message;
 #endif
 
 
